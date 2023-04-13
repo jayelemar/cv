4d68b2efbfcb550a5973f1a02a80c6b79f8b1f1f
Jay Termulo <jetermulo@gmail.com>
2023-04-13 22:57:38 +0800

    revision of navbar
    change in div container for the responsive mobile menu

--- main.js
+++ main.js
@@ -17,3 +17,30 @@ window.addEventListener("scroll", () => {
 
   header.classList.toggle("sticky", window.scrollY > 0)
 })
+
+// RESPONSIVE MOBILE MENU
+// 1)variables
+const hamburger = document.querySelector(".hamburger");
+const close = document.querySelector(".close");
+const menuWrapper = document.querySelector(".nav-wrapper");
+const resume = document.querySelector(".resume");
+
+// 3)create functions
+function showMenu() {
+    hamburger.style.display = "none";
+    resume.style.display = "none";
+    menuWrapper.style.transform = "translateX(100%)";
+
+}
+
+function hideMenu() {
+
+}
+
+
+
+
+// 2)Event Listener
+hamburger.addEventListener("click", showMenu);
+close.addEventListener("click", hideMenu);
+menuWrapper.addEventListener("click", hideMenu);
